98ce9d8175d2733aee8cfe1f3ee0caf1c2e64650
candopi-team1 <89337894+candopi-team1@users.noreply.github.com>
2021-08-22 18:05:39 +0800

    Add files via upload

---
+++ 5_cluelessness_welcome/multi_page_MCQs.js
@@ -0,0 +1,48 @@
+myChoice[item++] = new Choice(
+  'What is Starbuck?',
+  3,
+  'A kind of horse',
+  'Afootball club',
+  'A coffee brand'
+);
+
+myChoice[item++] = new Choice(
+  'Which tech company supports climate change?',
+  4,
+  'Aamazon',
+  'Intel',
+  'Google',
+  'None'
+)
+
+myChoice[item++] = new Choice(
+  'How do you open a bank account?',
+  2,
+  "Calling the bank'",
+  "Visiting a bank branch office",
+  "Going to a police station",
+  "Posting on Twitter or Facebook"
+)
+
+myChoice[item++] = new Choice(
+  'Can JavaScript count how many people visit a page?',
+  1,
+  'No',
+  'Yes',
+  'Only in Netscape Navigator'
+)
+
+
+myChoice[item++] = new Choice(
+  'Can we cure cancer?',
+  1,
+  'No',
+  'Yes',
+  'Only in Dreamland',
+  'Only in Russia'
+
+)
+
+// CLue: Copy any stanza above and edit properly.
+
+
