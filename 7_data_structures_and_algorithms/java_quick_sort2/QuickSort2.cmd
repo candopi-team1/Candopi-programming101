981017f8402270f5d51d22f75dbb2493d10af218
candopi-team1 <89337894+candopi-team1@users.noreply.github.com>
2021-08-28 14:33:34 +0800

    Add files via upload

---
+++ 7_data_structures_and_algorithms/java_quick_sort2/QuickSort2.cmd
@@ -0,0 +1,7 @@
+javac  -d  .    QuickSort2App.java 
+pause
+
+
+
+java    c7.sort.QuickSort2App
+pause
---
+++ 7_data_structures_and_algorithms/java_quick_sort3/Syntax1.java
@@ -0,0 +1,18 @@
+package blah;
+
+
+
+class BlahApp{
+   public static void main(String[] args)      {
+      
+     int pivot=10;
+     int leftPtr=0;
+     System.out.println("PREV= "+ leftPtr);
+     while( ++leftPtr < pivot ){
+       System.out.println(leftPtr);
+     }
+
+
+  }  // end main()
+   
+ }// end class BlahApp
